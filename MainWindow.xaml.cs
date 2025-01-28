72a169984e6c7aa0dc838d785bf70079f6c7f079
arnobw <arnobw@hotmail.com>
2025-01-28 16:21:46 +0100

    strokes need hit ignore

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,20 +12,15 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace wcanvas
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-    public partial class MainWindow : Window
+
+
+
+{    public partial class MainWindow : Window
     {
         public MainWindow()
         {
 
             InitializeComponent();
-            root.Title = "mooi hoor";
-            ic.UseCustomCursor = true;
-            root.Cursor = Cursors.Pen;
             ic.DefaultDrawingAttributes.Color = Colors.Yellow;
             ic.DefaultDrawingAttributes.Width = 20;
             ic.DefaultDrawingAttributes.Height = 20;
@@ -41,12 +37,25 @@ namespace wcanvas
             }
         }
 
+        private void Erase_b(object sender, RoutedEventArgs e) {
+            if (ic.EditingMode != InkCanvasEditingMode.EraseByPoint)
+            {
+                ic.EditingMode = InkCanvasEditingMode.EraseByPoint;
+
+            }
+            else {
+                ic.EditingMode = InkCanvasEditingMode.Ink;
+
+            }
+  
+        }
 
         private void Maximise_c(object sender, RoutedEventArgs e)
         {
             if (this.WindowState != WindowState.Maximized)
             {
                 this.WindowState = WindowState.Maximized;
+                
             }
             else if (this.WindowState != WindowState.Normal)
             {
@@ -54,6 +63,26 @@ namespace wcanvas
             }
         }
 
+        private void Clickthrough_c(object sender, RoutedEventArgs e) {
+            if (root.Topmost != true)
+            {
+                root.Topmost = true;
+                root.Background = null;
+
+                ic.EditingMode = InkCanvasEditingMode.None;
+                ic.IsHitTestVisible = false;
+                
+            }
+            else {
+            root.Background = (Brush)new BrushConverter().ConvertFrom("#01ffffff");
+                ic.IsHitTestVisible=true;
+
+                ic.EditingMode = InkCanvasEditingMode.Ink;
+
+
+            }
+            
+        }
 
         private void Close_c(object sender, RoutedEventArgs e)
         {
